87e5718334cd915df376bb4e3537ec764a75410f
Aftabul Islam <toaihimel@gmail.com>
2024-03-03 08:42:53 +0600

    feat: Adding review link on plugins.php page.

--- includes/Assets.php
+++ includes/Assets.php
@@ -15,10 +15,9 @@ class Assets {
      * @since 1.4.0
      */
     function __construct() {
-
         // Admin Assets
         add_action( 'admin_enqueue_scripts', [ $this, 'admin_asset' ] );
-
+        add_action( 'admin_enqueue_scripts', [ $this, 'admin_enqueue_asset' ] );
     }
 
     /**
@@ -39,6 +38,25 @@ class Assets {
             plugin_dir_url( WC_CALL_FOR_PRICE_PATH ) . 'assets/js/wcp-admin-script.js',
             [ 'jquery' ]
         );
+		wp_register_script(
+            'wcp-plugin-review-script',
+            plugin_dir_url( WC_CALL_FOR_PRICE_PATH ) . 'assets/js/plugin-list-page-review.js',
+            [ 'jquery' ]
+        );
     }
+
+	/**
+	 * Loads review script on plugins.php page
+	 *
+	 * @since 1.4.3
+	 *
+	 * @return void
+	 */
+	public function admin_enqueue_asset() {
+		global $pagenow;
+		if( 'plugins.php' === $pagenow ) {
+			wp_enqueue_script( 'wcp-plugin-review-script' );
+		}
+	}
 
 }
