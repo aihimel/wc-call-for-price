8ca30ef46efe93a63a9fc4f6d52e0f7f1aa28a95
Aftabul Islam <toaihimel@gmail.com>
2024-03-22 23:03:59 +0600

    feat: `OptionKeyInterface` added to implement option key string.

---
+++ includes/Models/Interfaces/OptionKeyInterface.php
@@ -0,0 +1,11 @@
+<?php
+namespace WCPress\WCP\Models\Interfaces;
+
+/**
+ * Option key interface to get option key
+ *
+ * @since WCP_SINCE
+ */
+interface OptionKeyInterface {
+	public function getOptionKey(): string;
+}
