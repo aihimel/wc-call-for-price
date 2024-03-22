c747349457730d3cc1d8beb0c6129e3ac25a88fe
Aftabul Islam <toaihimel@gmail.com>
2024-03-23 00:09:12 +0600

    feat: Model Class Updates * OptionKeyInterface support added * Empty data and incomplete data validation added on fetch

--- includes/Models/Model.php
+++ includes/Models/Model.php
@@ -2,19 +2,14 @@
 namespace WCPress\WCP\Models;
 
 use WCPress\WCP\Models\Interfaces\DefaultDataInterface;
+use WCPress\WCP\Models\Interfaces\OptionKeyInterface;
 
 /**
  * An abstract class to keep all common model functionality in one place
  *
  * @since 1.4.3
  */
-abstract class Model implements DefaultDataInterface {
-
-	/**
-	 * Option key id
-	 */
-	const OPTION_KEY = 'option_key';
-
+abstract class Model implements OptionKeyInterface, DefaultDataInterface {
 	/**
 	 * Option data
 	 *
@@ -32,7 +27,20 @@ abstract class Model implements DefaultDataInterface {
 	 * @return void
 	 */
 	public function __construct() {
-		$this->data = get_option( self::OPTION_KEY, $this->getDefaultData() );
+		$this->data = get_option( $this->getOptionKey(), $this->getDefaultData() );
+		$this->data = ( empty( $this->data ) || ! is_array( $this->data ) ) ? [] : $this->data; // For empty value
+		$this->data = array_merge( $this->getDefaultData(), $this->data ); // For incomplete data
+	}
+
+	/**
+	 * Return the name of the option key
+	 *
+	 * @since WCP_SINCE
+	 *
+	 * @return string
+	 */
+	public function getOptionKey(): string {
+		return 'wcp_option_key';
 	}
 
 	/**
@@ -43,7 +51,7 @@ abstract class Model implements DefaultDataInterface {
 	 * @return void
 	 */
 	public function save() {
-		update_option( self::OPTION_KEY, $this->data );
+		update_option( $this->getOptionKey(), $this->data );
 	}
 
 	/**
