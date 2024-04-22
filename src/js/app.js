134651cadbc16edcff25cae945af4818ef33b816
Aftabul Islam <toaihimel@gmail.com>
2024-04-22 15:57:13 +0600

    feat: State management example added

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,17 @@
+import { useState } from 'react';
+
+// @TODO test if new node package works
+
 export function App({}) {
+    const [ input_value, set_input_value ] = useState('');
+
+    const handleChange = (e) => {
+        set_input_value(e.target.value);
+    }
+
     return(
         <div>
-            Simple Dashbaord
+            <input type='text' value={input_value} onChange={handleChange}/><span>{input_value}</span>
         </div>
     );
 };
