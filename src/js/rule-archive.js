3222f17ed4693dd39d2db8f96ad45effe7d6842c
Aftabul Islam <toaihimel@gmail.com>
2024-04-24 21:21:18 +0600

    feat: Archive design updated.

--- src/js/rule-archive.js
+++ src/js/rule-archive.js
@@ -2,9 +2,25 @@ export default function RuleArchive({}){
     return(
         <div className='wcp-rule-archive-wrapper'>
             <div className='wcp-rule-custom-post'>
-                <div className='wcp-rule-post-header'>Post Title</div>
-                <div className='wcp-rule-post-content'>Post Description</div>
-                <div className='wcp-rule-post-footer'>Post Action</div>
+                <div className='wcp-rule-post-header'>
+                    <h3>Post Title</h3>
+                    <span className='wcp-rule-active'>Active</span>
+                </div>
+                <div className='wcp-rule-post-content'>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                </div>
+                <div className='wcp-rule-post-footer'>
+                    <ul>
+                        <li>
+                            <a href="" className='wcp-edit-rule'>Edit</a>
+                        </li>
+                        <li>
+                            <a href="" className='wcp-delete-rule'>Delete</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     );
