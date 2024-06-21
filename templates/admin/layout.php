8c2d063f3df5bd94b7dfe188ebbd66e8c9fe8ddf
Aftabul Islam <toaihimel@gmail.com>
2024-06-21 16:34:19 +0600

    feat: layout defined

--- templates/admin/layout.php
+++ templates/admin/layout.php
@@ -17,8 +17,18 @@ $wcp_is_page_valid = apply_filters( 'wcp_is_admin_subpage_valid', $wcp_sub_page 
 ?>
 
 <div class="wcp-admin-panel-wrapper">
-    <?php wcp_get_admin_template( 'parts/header.php' ); ?>
+    <div class="sidebar">
+      <div class="logo-wrapper">
+        <img
+          height="128"
+          width="128"
+          src="<?php echo plugin_dir_url( WC_CALL_FOR_PRICE_PATH ) . 'assets/images/wcpress-logo.png'?>"
+          alt="WCPress Logo"
+        >
+      </div>
+    </div>
     <div class="main">
+    		<?php wcp_get_admin_template( 'parts/header.php' ); ?>
         <?php
             if ( $wcp_is_page_valid ) {
                 wcp_get_admin_template( 'pages/' . $wcp_sub_page . '.php' );
@@ -26,6 +36,6 @@ $wcp_is_page_valid = apply_filters( 'wcp_is_admin_subpage_valid', $wcp_sub_page 
                 wcp_get_admin_template( 'pages/404.php' );
             }
         ?>
+    		<?php wcp_get_admin_template( 'parts/footer.php' ); ?>
     </div>
-    <?php wcp_get_admin_template( 'parts/footer.php' ); ?>
 </div>
