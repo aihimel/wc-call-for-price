cb51bad0f435a0b4dbdcd94794c6970881abb939
Aftabul Islam <toaihimel@gmail.com>
2025-07-01 20:44:23 +0600

    feat: update-readme script updated to automatically update the readme file

--- update-readme.sh
+++ update-readme.sh
@@ -1,7 +1,22 @@
-#! /bin/bash
-cp readme.txt .wordpress-org/trunk/readme.txt
-cp readme.txt .wordpress-org/tags/1.5.1/readme.txt
-cd .wordpress-org
-svn status
-svn ci -m 'readme.txt file updated'
-cd ..
+#!/bin/bash
+
+# Exit on error
+set -e
+
+# Define the stable tag
+STABLE_TAG=$(grep "^Stable tag:" readme.txt | awk -F ':' '{print $2}' | tr -d '[:space:]')
+
+# Define the source and destination paths
+SOURCE_FILE="readme.txt"
+TRUNK_DEST=".wordpress-org/trunk/readme.txt"
+TAG_DEST=".wordpress-org/tags/$STABLE_TAG/readme.txt"
+
+# Copy the readme.txt file to the trunk directory
+echo "Copying readme.txt to trunk..."
+cp "$SOURCE_FILE" "$TRUNK_DEST"
+
+# Copy the readme.txt file to the stable tag directory
+echo "Copying readme.txt to stable tag directory..."
+cp "$SOURCE_FILE" "$TAG_DEST"
+
+echo "Successfully updated readme.txt in .wordpress-org."
